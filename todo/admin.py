326d9e975538f4e1bdaa48b108752f6e95e7e38f
vinaycheekati <cheekativinay267@gmail.com>
2024-02-05 11:24:23 +0530

    adding todo to online repo

---
+++ todo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from todo.models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display=('task','is_completed','created_at','updated_at')
+    search_fields=('task',)
+admin.site.register(Task,TaskAdmin)
